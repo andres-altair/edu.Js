6f7ecd8acb62426155fac5dfd41ac22eeae78c92
Andres <andresxiaomd12@gmail.com>
2024-04-05 14:08:05 +0200

    no message

--- src/main/webapp/js.js
+++ src/main/webapp/js.js
@@ -94,3 +94,43 @@ console.log(dif);
 }
 
 
+
+
+
+
+
+
+
+/**
+*
+*/
+/*Probar el siguiente código con el script en el head y en el body. ¿Difiere en algo?*/
+const ruta = document.getElementById("separado");
+const fecha = new Date ();
+ruta.innerHTML=("<ul><li>"+fecha.getHours()+"</li><li>"+fecha.getMinutes()+"</li></ul>")
+function fhoy(){
+	 const dia = new Date();
+	 alert (dia);
+}
+function fmanyana(){
+	 const dia = new Date();
+	 console.log("El dia de hoy es: "+dia);
+	 const manyana = new Date();
+	 manyana.setDate(1);
+	 console.log(" con setDate(1): "+manyana);
+	 /*¿Qué hemos hecho? ¿Hemos sumado, restado u otra cosa?*/
+	 const mannana= new Date();
+	 mannana.setDate(dia.getDate()+1)
+	 console.log("con setDate(getdate+1): " +mannana);
+	 /*otra alternativa es la siguiente*/
+	 const otro = new Date();
+	 otro.setTime(dia.getTime()+24*60*60*1000);
+	 console.log(otro);
+	 /*otra alternativa para conseguir milisegundos es con Date.parse */
+	 manyana.setTime(Date.parse("2024/02/02")+24*60*60*1000);
+	 console.log(manyana);
+	 const feb = new Date("01/03/2024");
+	 console.log(feb.toString());
+}
+
+
